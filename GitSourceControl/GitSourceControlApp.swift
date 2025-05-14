56afa9b6ca7a9bfba5030a18a18595dc4801de17
Irfan Khan <ik950176@gmail.com>
2025-05-14 12:19:41 +0530

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by MAC on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
