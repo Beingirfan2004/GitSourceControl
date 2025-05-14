67a53900387b540a76823b350550977a32bf8ac4
Irfan Khan <ik950176@gmail.com>
2025-05-14 17:00:16 +0530

    [Patch] Fix Click Me button style to .bordered ( was .prominentBordered)

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             Button("Click Me!") {
                 
             }
-            .buttonStyle(.borderedProminent)
+            .buttonStyle(.bordered)
             Button("Subscribe") {
                 
             }
