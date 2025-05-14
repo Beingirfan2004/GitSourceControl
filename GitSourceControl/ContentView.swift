315a63a11633f56c9d1d73a5505e5c6173a20cc4
Irfan Khan <ik950176@gmail.com>
2025-05-14 16:46:22 +0530

    add click me button

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -14,7 +14,11 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking!")
-           
+            
+            Button("Click Me!") {
+                
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
