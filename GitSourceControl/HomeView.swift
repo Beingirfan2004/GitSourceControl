4bc221471259c6fb1e9eadd3d23662165ec33a01
Irfan Khan <ik950176@gmail.com>
2025-05-15 14:11:31 +0530

    [Feature] Title to Hello

--- GitSourceControl/HomeView.swift
+++ GitSourceControl/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State var title: String = "Second Title"
+    @State var title: String = "Hello World!"
     
     var body: some View {
         VStack {
